db987beea90d71cb66f8cc6346290da976141af1
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2022-03-28 16:28:44 +0530

    feat: updated colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 const blue = Color.fromRGBO(66, 134, 245, 1);
-const teal = Color.fromRGBO(18, 18, 18, 1);
-const green = Color.fromRGBO(38, 38, 38, 1);
-const yellow = Color.fromRGBO(0, 149, 246, 1);
-const orange = Colors.white;
-const red = Colors.grey;
-const brown = Color.fromRGBO(0, 149, 246, 1);
-const mool = Color.fromRGBO(0, 149, 246, 1);
+const teal = Color.fromRGBO(0, 152, 166, 1);
+const green = Color.fromRGBO(87, 159, 31, 1);
+const yellow = Color.fromRGBO(241, 163, 65, 1);
+const orange = Color.fromRGBO(243, 124, 56, 1);
+const red = Color.fromRGBO(242, 89, 75, 1);
+const brown = Color.fromRGBO(65, 1, 1, 1);
+const skin = Color.fromRGBO(255, 243, 227, 1);
