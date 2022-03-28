ad62c1b5adea744372a4a59a1b9f953f5da49666
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2022-03-28 16:21:09 +0530

    feat: added colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const blue = Color.fromRGBO(66, 134, 245, 1);
+const teal = Color.fromRGBO(18, 18, 18, 1);
+const green = Color.fromRGBO(38, 38, 38, 1);
+const yellow = Color.fromRGBO(0, 149, 246, 1);
+const orange = Colors.white;
+const red = Colors.grey;
+const brown = Color.fromRGBO(0, 149, 246, 1);
+const mool = Color.fromRGBO(0, 149, 246, 1);
