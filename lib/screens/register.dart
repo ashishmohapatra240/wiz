1c6d5813ddddd31bb9960d331fc5e0ac864bb99c
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2022-03-29 16:36:00 +0530

    Registration Page completed

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -30,110 +30,123 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(16, 64, 16, 64),
-          child: ListView(
-            children: [
-              Center(
-                child: Stack(
-                  children: [
-                    _image != null
-                        ? CircleAvatar(
-                            radius: 48, backgroundImage: MemoryImage(_image!))
-                        : const CircleAvatar(
-                            radius: 48,
-                            backgroundImage: NetworkImage(
-                              'https://www.linkpicture.com/q/Profile-Picture_2.png',
-                            ),
-                          ),
-                  ],
-                ),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 16),
-                ),
-                onPressed: () {
-                  selectImage();
-                },
-                child: const Text('Add Image'),
-              ),
-              const SizedBox(height: 36,),
-              TextFieldInput(
-                label: 'Name',
-                hintText: '',
-                textEditingController: _nameController,
-                textInputType: TextInputType.text,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFieldInput(
-                hintText: '',
-                textEditingController: _emailController,
-                textInputType: TextInputType.text, label: 'Email ID',
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFieldInput(
-                hintText: '',
-                textEditingController: _passwordController,
-                textInputType: TextInputType.text, label: 'Password', isPass: true,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFieldInput(
-                hintText: '',
-                textEditingController: _regdNoController,
-                textInputType: TextInputType.text, label: 'Registration Number',
-              ),
-              SizedBox(height: 16,),
-              InkWell(
-                // onTap: () async {
-                //   String res = await AuthMethods().signupUser(
-                //     email: _emailController.text,
-                //     password: _passwordController.text,
-                //     username: _usernameController.text,
-                //     bio: _bioController.text,
-                //     file: _image!,
-                //   );
-                // },
-                child: Container(
-                  child: const Text('Sign up',),
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.symmetric(vertical: 12),
-                  decoration: const ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(4.0),
-                      ),
-                    ),
-                    color: blue,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 12.0,
-              ),
-              Flexible(
-                child: Container(),
-                flex: 2,
-              ),
-              //transitioning to signup
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Scaffold(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(16, 64, 16, 64),
+        child: ListView(
+          children: [
+            Center(
+              child: Stack(
                 children: [
-                  Container(
-                    child: const Text("Don't have an account?"),
-                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  _image != null
+                      ? CircleAvatar(
+                          radius: 48, backgroundImage: MemoryImage(_image!))
+                      : const CircleAvatar(
+                          radius: 48,
+                          backgroundImage: NetworkImage(
+                            'https://www.linkpicture.com/q/Profile-Picture_2.png',
+                          ),
+                        ),
+                ],
+              ),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 16),
+              ),
+              onPressed: () {
+                selectImage();
+              },
+              child: const Text('Add Image'),
+            ),
+            const SizedBox(
+              height: 36,
+            ),
+            TextFieldInput(
+              label: 'Name',
+              hintText: '',
+              textEditingController: _nameController,
+              textInputType: TextInputType.text,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFieldInput(
+              hintText: '',
+              textEditingController: _emailController,
+              textInputType: TextInputType.text,
+              label: 'Email ID',
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFieldInput(
+              hintText: '',
+              textEditingController: _passwordController,
+              textInputType: TextInputType.text,
+              label: 'Password',
+              isPass: true,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFieldInput(
+              hintText: '',
+              textEditingController: _regdNoController,
+              textInputType: TextInputType.text,
+              label: 'Registration Number',
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            InkWell(
+              // onTap: () async {
+              //   String res = await AuthMethods().signupUser(
+              //     email: _emailController.text,
+              //     password: _passwordController.text,
+              //     username: _usernameController.text,
+              //     bio: _bioController.text,
+              //     file: _image!,
+              //   );
+              // },
+              child: Container(
+                height: 56,
+                child: const Text(
+                  'Sign up',
+                  style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),
+                ),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(4.0),
+                    ),
                   ),
-            ],
-          ),
-            ],),
+                  color: blue,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            Flexible(
+              child: Container(),
+              flex: 2,
+            ),
+            //transitioning to signup
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: const Text("Don't have an account?"),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     ));
   }
