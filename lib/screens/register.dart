5f25589ace902116418886fd3970131887ac5a86
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2022-03-29 16:31:00 +0530

    Added Sign up Button

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:wiz/utils/colors.dart';
 import 'package:wiz/utils/utils.dart';
 import 'package:wiz/widgets/text_field_imput.dart';
 
@@ -58,7 +59,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 },
                 child: const Text('Add Image'),
               ),
-              SizedBox(height: 36,),
+              const SizedBox(height: 36,),
               TextFieldInput(
                 label: 'Name',
                 hintText: '',
@@ -89,10 +90,51 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 textEditingController: _regdNoController,
                 textInputType: TextInputType.text, label: 'Registration Number',
               ),
+              SizedBox(height: 16,),
+              InkWell(
+                // onTap: () async {
+                //   String res = await AuthMethods().signupUser(
+                //     email: _emailController.text,
+                //     password: _passwordController.text,
+                //     username: _usernameController.text,
+                //     bio: _bioController.text,
+                //     file: _image!,
+                //   );
+                // },
+                child: Container(
+                  child: const Text('Sign up',),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(4.0),
+                      ),
+                    ),
+                    color: blue,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+              //transitioning to signup
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: const Text("Don't have an account?"),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  ),
             ],
           ),
-        ),
+            ],),
       ),
-    );
+    ));
   }
 }
